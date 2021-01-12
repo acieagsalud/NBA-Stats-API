aadffcfc7e0a5aca489e2443e6b04af7a2860881
acieagsalud <57071675+acieagsalud@users.noreply.github.com>
2021-01-12 15:18:00 -0600

    Created Player and Team models

---
+++ NBA Stats API/Models/Team.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NBA_Stats_API.Models
+{
+    public class Team
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string City { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Abbreviation { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        public string Conference { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        public string Division { get; set; }
+    }
+}
