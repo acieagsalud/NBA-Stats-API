29b9524aedb924800687f72d12d0200a11e57244
acieagsalud <57071675+acieagsalud@users.noreply.github.com>
2021-01-12 21:49:38 -0600

    Created Profile Mapping for Players and Teams

---
+++ NBA Stats API/Profiles/TeamsProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using NBA_Stats_API.Dtos;
+using NBA_Stats_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NBA_Stats_API.Profiles
+{
+    public class TeamsProfile : Profile
+    {
+        public TeamsProfile()
+        {
+            CreateMap<Team, TeamReadDto>();
+        }
+    }
+}
