a5a7b9b49a5875ad3133c46fdd9ffb952ada25b2
acieagsalud <57071675+acieagsalud@users.noreply.github.com>
2021-01-12 21:47:07 -0600

    Added Controllers for Players and Teams

---
+++ NBA Stats API/Controllers/PlayersController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NBA_Stats_API.Data;
+using NBA_Stats_API.Dtos;
+using NBA_Stats_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NBA_Stats_API.Controllers
+{
+    [Route("api/players")]
+    [ApiController]
+    public class PlayersController : ControllerBase
+    {
+        private readonly INBARepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlayersController(INBARepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        // GET api/players
+        [HttpGet]
+        public ActionResult<IEnumerable<Player>> GetAllPlayers()
+        {
+            var players = _repository.GetAllPlayers();
+
+            return Ok(_mapper.Map<IEnumerable<PlayerReadDto>>(players));
+        }
+
+        // GET api/players/{id}
+        [HttpGet("{id}", Name="GetPlayerById")]
+        public ActionResult<PlayerReadDto> GetPlayerById(int id)
+        {
+            var player = _repository.GetPlayerById(id);
+
+            if (player != null) return Ok(_mapper.Map<PlayerReadDto>(player));
+            else return NotFound();
+        }
+    }
+}
---
+++ NBA Stats API/Controllers/TeamsController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NBA_Stats_API.Data;
+using NBA_Stats_API.Dtos;
+using NBA_Stats_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NBA_Stats_API.Controllers
+{
+    [Route("api/teams")]
+    [ApiController]
+    public class TeamsController : ControllerBase
+    {
+        private readonly INBARepo _repository;
+        private readonly IMapper _mapper;
+
+        public TeamsController(INBARepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        // GET api/teams
+        [HttpGet]
+        public ActionResult<IEnumerable<Team>> GetAllTeams()
+        {
+            var teams = _repository.GetAllTeams();
+
+            return Ok(_mapper.Map<IEnumerable<TeamReadDto>>(teams));
+        }
+
+        // GET api/teams/{id}
+        [HttpGet("{id}", Name = "GetTeamById")]
+        public ActionResult<PlayerReadDto> GetTeamById(int id)
+        {
+            var team = _repository.GetTeamById(id);
+
+            if (team != null) return Ok(_mapper.Map<TeamReadDto>(team));
+            else return NotFound();
+        }
+    }
+}
