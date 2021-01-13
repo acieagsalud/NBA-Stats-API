fadb9c8b9d3762bc835b788510801d31d34ae2eb
acieagsalud <57071675+acieagsalud@users.noreply.github.com>
2021-01-12 21:49:11 -0600

    Created Data Transfer Objects for Players and Teams

---
+++ NBA Stats API/Dtos/PlayerReadDto.cs
@@ -0,0 +1,27 @@
+﻿using NBA_Stats_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NBA_Stats_API.Dtos
+{
+    public class PlayerReadDto
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Position { get; set; }
+
+        public string Height { get; set; }
+
+        public int Weight { get; set; }
+
+        public int TeamId { get; set; }
+
+        public Team Team { get; set; }
+    }
+}
---
+++ NBA Stats API/Dtos/TeamReadDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NBA_Stats_API.Dtos
+{
+    public class TeamReadDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+        public string Abbreviation { get; set; }
+
+        public string Conference { get; set; }
+
+        public string Division { get; set; }
+    }
+}
