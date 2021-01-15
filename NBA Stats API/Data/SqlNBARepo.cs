f7d0f85d3c7fde1743ff2ef7f65c5b863708b54e
acieagsalud <57071675+acieagsalud@users.noreply.github.com>
2021-01-15 16:27:46 -0600

    Organized SqlNBARepo to organize methods by controller

--- NBA Stats API/Data/SqlNBARepo.cs
+++ NBA Stats API/Data/SqlNBARepo.cs
@@ -14,13 +14,24 @@ namespace NBA_Stats_API.Data
             _context = context;
         }
 
+        // Players Controller
         public IEnumerable<Player> GetAllPlayers()
         {
             return _context.Players
                 .Include(t => t.Team)
                 .ToList();
         }
-
+        public Player GetPlayerById(int id)
+        {
+            return _context.Players.FirstOrDefault(p => p.Id == id);
+        }
+        public IEnumerable<Player> GetPlayerByName(string name)
+        {
+            return _context.Players
+                .Include(t => t.Team)
+                .Where(p => p.FirstName.Contains(name) || p.LastName.Contains(name))
+                .ToList();
+        }
         public IEnumerable<Player> GetAllPlayersOnTeamId(int id)
         {
             return _context.Players
@@ -36,28 +47,15 @@ namespace NBA_Stats_API.Data
                 .ToList();
         }
 
+        // Teams Controller
         public IEnumerable<Team> GetAllTeams()
         {
             return _context.Teams.ToList();
         }
-
-        public Player GetPlayerById(int id)
-        {
-            return _context.Players.FirstOrDefault(p => p.Id == id);
-        }
-        public IEnumerable<Player> GetPlayerByName(string name)
-        {
-            return _context.Players
-                .Include(t => t.Team)
-                .Where(p => p.FirstName.Contains(name) || p.LastName.Contains(name))
-                .ToList();
-        }
-
         public Team GetTeamById(int id)
         {
             return _context.Teams.FirstOrDefault(t => t.Id == id);
         }
-
         public IEnumerable<Team> GetTeamByName(string name)
         {
             return _context.Teams.Where(t => t.Name.Contains(name)).ToList();
